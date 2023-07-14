144e632f777f8322831133ea38f092002b9c2f81
Filadelico <nestor_05ribero@hotmail.com>
2023-07-13 19:08:09 -0500

    Create Primera.java

---
+++ Julio-12/Primera.java
@@ -0,0 +1,23 @@
+public class Primera {
+
+  public static void main(String[] args) {
+    /*
+     * var a=5;
+     * var b=8;
+     * console.log(a+b)
+     */
+    //PRIMITIVAS
+    int a = 5;
+    int b = 8;
+    System.out.println(a + b);
+    char genero = 'm';
+    double c = 2.2;
+    boolean k = false;
+
+    int[] nums = { 1, 2, 3, 4, 5 };
+    char[] palabra = { 'p', 'e', 'r', 'r', 'o' };
+
+    int[] edades= new int[5];
+
+  }
+}
