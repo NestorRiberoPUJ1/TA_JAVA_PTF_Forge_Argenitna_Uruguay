d16a9a6a6da0a17723c24b2d2e7ec1418309d268
Filadelico <nestor_05ribero@hotmail.com>
2023-07-17 19:04:49 -0500

    Update Primera.java

--- Julio-12/Primera.java
+++ Julio-12/Primera.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Primera {
 
   public static void main(String[] args) {
@@ -17,7 +20,34 @@ public class Primera {
     int[] nums = { 1, 2, 3, 4, 5 };
     char[] palabra = { 'p', 'e', 'r', 'r', 'o' };
 
-    int[] edades= new int[5];
+    int[] edades = new int[5];
 
+    //No PRIMITIVAS
+    Integer numero_suerte = 5;
+
+    String nombre = "Nestor";
+
+    Double[] lista_nums = { 1.0, 2.8, 3.42 };
+
+    //Listas de arreglos y los mapas de hash
+
+    ArrayList<Integer> myArray = new ArrayList<Integer>();
+
+    myArray.add(1);
+    myArray.add(2);
+    myArray.add(3);
+    myArray.add(4);
+
+    myArray.remove(3);
+
+    ArrayList<Integer> numeros = new ArrayList<Integer>();
+    numeros.add(5);
+    numeros.add(4);
+    numeros.add(3);
+    numeros.add(2);
+    numeros.add(1);
+
+    Collections.shuffle(numeros);
+    System.out.println(numeros); // [5, 4, 3, 2, 1]
   }
 }
